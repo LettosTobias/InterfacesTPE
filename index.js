d88c428f887ec44301f90c3b2830d0bb981a487c
Lautaro Scuffi <scuffilautaro@gmail.com>
2023-11-24 12:15:45 -0300

    agrega efecto fade in

--- index.js
+++ index.js
@@ -21,3 +21,36 @@ window.onscroll = function () {
     logo.style.maxWidth = "100%"; // Restaura el tamaño de la imagen original
   }
 };
+
+// Obtén las referencias a los elementos
+let cards = document.querySelectorAll(".spidersCards");
+
+// Función para verificar si un elemento está en el viewport
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Función para manejar el scroll
+function handleScroll() {
+  cards.forEach((card) => {
+    if (isElementInViewport(card)) {
+      card.classList.add("visible");
+    }
+    if (window.scrollY > 3000 || window.scrollY < 1000) {
+      card.classList.remove("visible");
+    }
+  });
+}
+
+// Adjunta el evento de scroll
+window.addEventListener("scroll", handleScroll);
+
+// Llama a handleScroll al cargar la página para verificar los elementos iniciales
+handleScroll();
